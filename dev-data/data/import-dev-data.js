3ada3930d364957c9da1a86faaa08524e8c5a122
ManjinderSingh131 <smanjinder576@gmail.com>
2021-02-21 12:53:05 +0530

    host netlify final

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -26,7 +26,7 @@ const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
 const importData = async () => {
   try {
     await Tour.create(tours);
-    await User.create(users, { validateBeforeSave: false });
+//    await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
